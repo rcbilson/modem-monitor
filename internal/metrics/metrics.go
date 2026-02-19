afccce8985c328674c12c66f882741e69449da98
Richard Bilson <rcbilson@gmail.com>
2026-02-19 01:51:27 +0000

    Better state metric

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -6,17 +6,11 @@ import (
 )
 
 var (
-	// Labeled gauge for compatibility with existing queries
-	stateGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+	// Current state info metric
+	stateGauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "modem_monitor_state",
-		Help: "Current state of the modem monitor (1=active, 0=inactive)",
-	}, []string{"state"})
-
-	// Current state info metric with state as label (for Grafana state timeline)
-	stateInfo = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "modem_monitor_state_info",
-		Help: "Current state of the modem monitor (always 1, check state label)",
-	}, []string{"state"})
+		Help: "Current state of the modem monitor (0=operating 1=investigating 2=resetting 3=recovering)",
+	})
 
 	StateTransitions = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "modem_monitor_state_transitions_total",
@@ -38,25 +32,15 @@ var (
 		Help: "Total number of failed ping rounds",
 	})
 
-	allStates  = []string{"operating", "investigating", "resetting", "recovering"}
-	lastState  string
+	allStates = []string{"operating", "investigating", "resetting", "recovering"}
 )
 
 // SetState sets the active state gauge. The given state is set to 1,
 // all others to 0. Also updates the state info metric.
 func SetState(state string) {
-	for _, s := range allStates {
+	for i, s := range allStates {
 		if s == state {
-			stateGauge.WithLabelValues(s).Set(1)
-		} else {
-			stateGauge.WithLabelValues(s).Set(0)
+			stateGauge.Set(float64(i))
 		}
 	}
-
-	// Clear previous state info, set current state to 1
-	if lastState != "" {
-		stateInfo.DeleteLabelValues(lastState)
-	}
-	stateInfo.WithLabelValues(state).Set(1)
-	lastState = state
 }
